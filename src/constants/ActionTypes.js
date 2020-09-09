d09d5bef7dc8c918ab6d9b8fd21635972364c4ae
Arnaud Kapongo <kapongoarnaud@gmail.com>
2020-09-09 14:05:50 +0200

    commit-demineur

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -5,13 +5,5 @@ export const YOU_LOOSE = 'YOU_LOOSE';
 export const YOU_WIN = 'YOU_WIN';
 export const VISIBLE_CELL = 'VISIBLE_CELL';
 export const GENERATE_GRID = 'GENERATE_GRID';
-
-
-
-export const MARK_COMMAND = 'MARK_COMMAND';
-export const REVEAL_COMMAND = 'REVEAL_COMMAND';
-export const GUESS_COMMAND = 'GUESS_COMMAND';
-export const MARK_CELL = 'MARK_CELL';
 export const REVEAL_CELL = 'REVEAL_CELL';
-export const GUESS_CELL = 'GUESS_CELL';
 export const GAME_OVER = 'GAME_OVER';
